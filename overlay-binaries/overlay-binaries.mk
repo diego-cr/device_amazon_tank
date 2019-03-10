f127f817714bdfe2835db418e6c848d386c2a214
diego-cr <diegocr@gmail.com>
2019-03-10 23:52:04 +0100

    update Overlay Binaries
    Change-Id: I35595600087347b6508c99678129d28a7804e89f

--- overlay-binaries/overlay-binaries.mk
+++ overlay-binaries/overlay-binaries.mk
@@ -1,15 +1,5 @@
 OVERLAY_BIN := device/amazon/tank/overlay-binaries
 
-# Install module to set selinux to permissive
-# Loaded by init.mt8127.rc
-PRODUCT_COPY_FILES += \
-	$(OVERLAY_BIN)/selinux_permissive.ko:system/lib/modules/vcodec_kernel_driver.ko
-
-# install-recovery.sh used to initialise init.d support	
-PRODUCT_COPY_FILES += \
-	$(OVERLAY_BIN)/install-recovery.sh:system/bin/new-install-recovery.sh \
-	$(OVERLAY_BIN)/install-recovery.sh:system/etc/install-recovery-2.sh
-	
 # Install init.d scripts
 PRODUCT_COPY_FILES += \
 	$(OVERLAY_BIN)/99exfat-support:system/etc/init.d/99exfat-support \
