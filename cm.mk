c5b0e0093a8020d7b0a36a621987e5514dde3a02
diego-cr <diegocr@gmail.com>
2019-08-19 19:15:08 +0200

    Add boot.img

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
-# Inherit device configuration for mt8127-common
-$(call inherit-product, device/amazon/mt8127-common/cm.mk)
+# Inherit some common CM stuff.
+$(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/amazon/tank/full_tank.mk)
 
-PRODUCT_CHARACTERISTICS := tablet
+#PRODUCT_CHARACTERISTICS := tablet
 
 TARGET_SCREEN_WIDTH := 600
 TARGET_SCREEN_HEIGHT := 1024
